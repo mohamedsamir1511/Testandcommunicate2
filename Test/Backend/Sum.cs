a2ac6e8bdcabb6cf352ccba2491caeda68bd8a07
Mohamed Samir Alqadi <MAlqadi@gc.local>
2025-09-07 14:47:01 +0300

    Add Sum.cs class to Backend

---
+++ Test/Backend/Sum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Backend
+{
+    internal class Sum
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }   
+    }
+}
